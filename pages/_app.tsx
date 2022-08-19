5b9c2ed0cb0840a8a12b56b2cd16ef35bf1869c5
gregfromstl <gregfromstl@gmail.com>
2022-08-19 18:44:41 -0500

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,48 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ViewGridIcon } from "@heroicons/react/outline";
+import { Nation3App, DefaultLayout, DefaultSidebar } from "@nation3/components";
+import type { AppProps } from "next/app";
+import logo from "/logo.svg";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function Nation3({ Component, pageProps }: AppProps) {
+  return (
+    <Nation3App>
+      <DefaultLayout
+        sidebar={
+          <DefaultSidebar
+            onConnect={console.log}
+            logo={<img src="/logo.svg" alt="Nation3 Logo" />}
+            onRoute={console.log}
+            navLinks={[
+              {
+                route: "/",
+                icon: <ViewGridIcon className="w-5 h-5" />,
+                name: "Start",
+                isActive: true,
+              },
+            ]}
+            connectors={[
+              {
+                name: "MetaMask",
+                id: "ADUBO",
+                ready: true,
+              },
+            ]}
+            account={{
+              address: "0x540ABe72",
+              connector: {
+                name: "MetaMask",
+                id: "ADUBO",
+                ready: true,
+              },
+            }}
+            onDisconnect={console.log}
+          />
+        }
+      >
+        <Component {...pageProps} />
+      </DefaultLayout>
+    </Nation3App>
+  );
 }
 
-export default MyApp
+export default Nation3;
