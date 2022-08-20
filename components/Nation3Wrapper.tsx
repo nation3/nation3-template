371193196307e79c1b76888a597c9936dd0bde7f
gregfromstl <gregfromstl@gmail.com>
2022-08-19 19:18:47 -0500

    feat(Nation3Wrapper): connect wagmi

--- components/Nation3Wrapper.tsx
+++ components/Nation3Wrapper.tsx
@@ -1,17 +1,26 @@
 import { ViewGridIcon } from "@heroicons/react/outline";
 import { Nation3App, DefaultLayout, DefaultSidebar } from "@nation3/components";
+import { useAccount, useConnect, useDisconnect } from "wagmi";
 
 function Nation3Wrapper({
   children,
 }: {
   children: React.ReactElement | React.ReactElement[];
 }) {
+  const { address, connector } = useAccount();
+  const { connect, connectors } = useConnect();
+  const { disconnect } = useDisconnect();
+
   return (
     <Nation3App>
       <DefaultLayout
         sidebar={
           <DefaultSidebar
-            onConnect={console.log}
+            onConnect={(connector) => {
+              connect({
+                connector: connectors.find((i) => i.name === connector.name),
+              });
+            }}
             logo={<img src="/logo.svg" alt="Nation3 Logo" />}
             onRoute={console.log}
             navLinks={[
@@ -22,22 +31,18 @@ function Nation3Wrapper({
                 isActive: true,
               },
             ]}
-            connectors={[
-              {
-                name: "MetaMask",
-                id: "ADUBO",
-                ready: true,
-              },
-            ]}
-            account={{
-              address: "0x540ABe72",
-              connector: {
-                name: "MetaMask",
-                id: "ADUBO",
-                ready: true,
-              },
-            }}
-            onDisconnect={console.log}
+            connectors={connectors.map((connector) => ({
+              ...connector,
+            }))}
+            account={
+              address && connector
+                ? {
+                    address,
+                    connector,
+                  }
+                : undefined
+            }
+            onDisconnect={disconnect}
           />
         }
       >
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,20 @@
-import { ViewGridIcon } from "@heroicons/react/outline";
-import { Nation3App, DefaultLayout, DefaultSidebar } from "@nation3/components";
+import { getDefaultProvider } from "ethers";
 import type { AppProps } from "next/app";
+import { createClient, WagmiConfig } from "wagmi";
 import Nation3Wrapper from "../components/Nation3Wrapper";
+
+const client = createClient({
+  autoConnect: true,
+  provider: getDefaultProvider(),
+});
 
 function Nation3({ Component, pageProps }: AppProps) {
   return (
-    <Nation3Wrapper>
-      <Component {...pageProps} />
-    </Nation3Wrapper>
+    <WagmiConfig client={client}>
+      <Nation3Wrapper>
+        <Component {...pageProps} />
+      </Nation3Wrapper>
+    </WagmiConfig>
   );
 }
 
